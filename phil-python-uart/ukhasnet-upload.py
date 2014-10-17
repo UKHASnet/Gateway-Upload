19b3fe4cdf2ac2cbf2c77acd5c0395515e8fc867
Mat Burnham <matburnham+gitcommit@gmail.com>
2014-10-17 21:32:52 +0100

    Timestamp the log Simplify upload_data and upload_data_rssi into one function Warn if upload disabled

--- phil-python-uart/ukhasnet-upload.py
+++ phil-python-uart/ukhasnet-upload.py
@@ -2,26 +2,17 @@
 import serial
 import httplib,urllib
 import sys
+import datetime
 
 # Callsign of gateway node
 gateway_callsign='CHANGEME'
 # Should we upload to ukhas.net? Disable for debugging
 upload=False
 
-def upload_data( line ):
+def upload_data( line, rssi=None ):
   params = urllib.urlencode({'origin': gateway_callsign, 'data': line})
-  headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
-  conn = httplib.HTTPSConnection("www.ukhas.net")
-  conn.request("POST", "/api/upload", params, headers)
-  response = conn.getresponse()
-  data = response.read()
-  if(response.status!=200):
-    print 'Upload Failed: '+data
-  conn.close()
-  return
-
-def upload_data_rssi( line, rssi ):
-  params = urllib.urlencode({'origin': gateway_callsign, 'data': line, 'rssi': rssi})
+  if rssi:
+    params['rssi'] = rssi
   headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
   conn = httplib.HTTPSConnection("www.ukhas.net")
   conn.request("POST", "/api/upload", params, headers)
@@ -37,6 +28,9 @@ if(gateway_callsign=='CHANGEME'):
     print 'Exiting...'
     sys.exit()
 
+if not upload:
+    print 'WARNING: Upload disabled'
+
 node = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
 
 try:
@@ -44,10 +38,10 @@ try:
        try:
         data_line = node.readline().rstrip()
         if(data_line.__len__() > 0):
-	    print(data_line)
+	    print(str(datetime.datetime.now()) + ' ' + data_line)
 	    if upload:
 	        if "|" in data_line: # Check for RSSI
-	            upload_data_rssi(data_line.split("|")[0],data_line.split("|")[1])
+	            upload_data(data_line.split("|")[0],data_line.split("|")[1])
 	        else:
 	            upload_data(data_line)
        except Exception, e:
